4462e356b827f3fe9230513c170631ddafdb2ee1
Andres Giraldo <actorgiraldo@gmail.com>
2015-01-30 13:55:44 -0800

    NEW UPDATE

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,8 +1,13 @@
 <?php
+    require_once (__DIR__ . "/../model/database.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    $connection = new mysqli($host, $username, $password);
+            
+            if($connection->connect_error) {
+            die("Error: " . $connection->connect_error);
+        }
+        else{
+            echo "success: " . $connection->host_info;
+        }
+        
+        $connection->close();
