7b573236b0ae52bce0086168d3e0a87c5212e563
Andres Giraldo <actorgiraldo@gmail.com>
2015-02-03 08:00:21 -0800

    new update

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,13 +1,23 @@
 <?php
-    require_once (__DIR__ . "/../model/database.php");
 
-    $connection = new mysqli($host, $username, $password);
-            
-            if($connection->connect_error) {
-            die("Error: " . $connection->connect_error);
-        }
-        else{
-            echo "success: " . $connection->host_info;
-        }
-        
-        $connection->close();
+require_once (__DIR__ . "/../model/database.php");
+
+$connection = new mysqli($host, $username, $password);
+
+if ($connection->connect_error) {
+    die("Error: " . $connection->connect_error);
+}
+
+$exists = $connection->select_db($database);
+
+if (!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+
+    if ($query) {
+        echo "sucessfuly created a database: " . $database;
+    }
+}
+
+
+
+$connection->close();
